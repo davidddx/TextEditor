2baefc9125748893dcb5f0780ceb629bef6a7440
davidddx <david0319072@gmail.com>
2025-07-24 00:30:52 -0400

    added debug header

---
+++ include/debug.h
@@ -0,0 +1,11 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+typedef struct {
+        float fps;
+        int window_width;
+        int window_height;
+        int desktop_width;
+        int desktop_height;
+
+} Debug_Info;
+#endif
